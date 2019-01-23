578c7f67efac928f9f88eb74aee0429aa6622554
Democke <xerorythic@gmail.com>
2019-01-22 19:13:03 -0800

    Add RPpal to project dependencies, add blinking demo for pin 19

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use rppal::gpio::Gpio;
+use std::thread;
+use std::time::Duration;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello World!");
-}
+    let mut pin = Gpio::new()?.get(19)?.into_output();
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+    Ok(())
+    }
+    //13,19,26
+
